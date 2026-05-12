2e92c38ea0f33696f34220c92ab28df542f0dfbe
rochelle-dmello <salsachickenwrap@gmail.com>
2026-05-13 00:44:53 +0530

    password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -7,13 +7,19 @@ print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
-password =[]
-for i in range(1,nr_letters+1):
-    password += random.choice(letters)
-for i in range(1, nr_symbols + 1):
-    password += random.choice(symbols)
-for i in range(1,nr_numbers +1):
-    password += random.choice(numbers)
-random.shuffle(password)
-new_password = ''.join(password)
-print(new_password)
+password_list = []
+for char in range(0, nr_letters):
+    password_list.append(random.choice(letters))
+
+for char in range(0, nr_symbols):
+    password_list.append(random.choice(symbols))
+
+for char in range(0, nr_numbers):
+    password_list.append(random.choice(numbers))
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+new_password = ''.join(password_list)
+print(f"You're new password is: {new_password}")
